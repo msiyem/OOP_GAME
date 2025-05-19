eb515f45f79864162dbd10cb7bf40b292a8366f2
MD MAHFUJUL HAQUE SIYEM <147250959+msiyem@users.noreply.github.com>
2025-05-19 18:45:57 +0600

    SmallCactusDouble.java

---
+++ core/src/main/java/com/mygdx/dinogame/SmallCactusDouble.java
@@ -0,0 +1,8 @@
+package com.mygdx.dinogame;
+
+public class SmallCactusDouble extends Obstacle {
+    public SmallCactusDouble(DinoGame game, float speedMultiplier) {
+        super(game,speedMultiplier);
+        setTextureAndSize("cactus_double.png",60,60);
+    }
+}
